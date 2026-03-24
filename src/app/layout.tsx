c1336bbeb701c439f03ef979f6584b818e19e3aa
michaeltio <tio.michael2004@gmail.com>
2026-03-25 01:54:16 +0700

    add toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "@/providers/CartProvider";
 import { ThemeProvider } from "@/providers/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
           enableSystem={false}
         >
           <CartProvider>{children}</CartProvider>
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
